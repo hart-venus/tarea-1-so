55ab068506fd66ba6afdb119ec0de72f0ca23820
Ariel Leyva <aleyva1509@gmail.com>
2024-02-23 21:46:56 -0600

    add: exercise 1 solution

--- Tarea1_1-2022175018.c
+++ Tarea1_1-2022175018.c
@@ -1,6 +1,34 @@
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main () {
+    int i;
+    int N_HIJOS= 5;
+    int N_GENERACIONES = 3;
+    int gen = 1; 
+
+    for (int j = 0; j < N_GENERACIONES; j++) {
+        for (i = 0; i < N_HIJOS; i++) {
+            int pid = fork();
+            if (pid != 0) {
+                wait(NULL); // esperar a que el hijo termine
+            } else {
+                gen++;
+                break; // soy el hijo, ocupo continuar con el ciclo exterior 
+            }
+        }
+        if (i == N_HIJOS) {
+            break; // soy el padre, ocupo salir del ciclo exterior
+        }
+    }    
+
+    printf("Gen: %d \t Padre: %ld \t Yo: %ld \n", gen, (long)getppid(), (long)getpid());
+    // Se comprobó el funcionamiento con grep, ex.
+    // ./Tarea1_1-2022175018 | grep -c "Gen: 4" -> 125
+    // ./Tarea1_1-2022175018 | grep -c "Gen: 3" -> 25
+    // ./Tarea1_1-2022175018 | grep -c "Gen: 2" -> 5
+    // ./Tarea1_1-2022175018 | grep -c "Gen: 1" -> 1
+    exit(0);
 }
